98fbad29c7af45ee7814593f09d57a07ae72dd24
Phong Le <ltphong1988@gmail.com>
2019-08-08 01:28:56 +0700

    deo hieu

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'angular')));
 // use body parser so we can get info from POST and/or URL parameters
 // app.use(cookieParser());
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 // app.use(csrf({ cookie: true }));
